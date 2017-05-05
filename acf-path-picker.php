4922dfd29df9dd5f49f2465f1e676361ee3c5500
Mike Walker <mike@incredimike.com>
2017-05-05 15:45:56 -0700

    enable actual plugin

--- acf-path-picker.php
+++ acf-path-picker.php
@@ -25,14 +25,14 @@
 // load_plugin_textdomain( 'acf-fs-path-picker', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
-// // 2. Include field type for ACF5
-// // $version = 5 and can be ignored until ACF6 exists
-// function include_field_types_fs_path_picker( $version ) {
+// 2. Include field type for ACF5
+// $version = 5 and can be ignored until ACF6 exists
+function include_field_types_fs_path_picker( $version ) {
 
-// 	include_once('acf-fs-path-picker-v5.php');
+	include_once('acf-fs-path-picker-v5.php');
 
-// }
+}
 
-// add_action('acf/include_field_types', 'include_field_types_fs_path_picker');
+add_action('acf/include_field_types', 'include_field_types_fs_path_picker');
 
 
