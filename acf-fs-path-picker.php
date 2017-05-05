d85f30e56eec3e9f75e2094fb242f96a5d376a3c
Mike Walker <mike@incredimike.com>
2017-05-05 12:57:59 -0700

    more verbose description

--- acf-fs-path-picker.php
+++ acf-fs-path-picker.php
@@ -1,21 +1,38 @@
 <?php
-/*
-Plugin Name: Advanced Custom Fields: Filesystem Path Picker Field
-*/
+/**
+ * @package   Plugin_Name
+ * @author    Mike Walker <mike@incredimike.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2017 incredimike development services
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Advanced Custom Fields: Filesystem Path Picker Field
+ * Plugin URI:        @TODO
+ * Description:       Create a field that returns a path to a file.
+ * Version:           1.0.0
+ * Author:            Mike Walker
+ * Author URI:        http://incredimike.com
+ * Text Domain:       acf-fs-path-picker
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /lang
+ * GitHub Plugin URI: https://github.com/incredimike/acf-fs-path-picker
+ */
 
 // 1. set text domain
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-fs-path-picker', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+// load_plugin_textdomain( 'acf-fs-path-picker', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
-// 2. Include field type for ACF5
-// $version = 5 and can be ignored until ACF6 exists
-function include_field_types_fs_path_picker( $version ) {
+// // 2. Include field type for ACF5
+// // $version = 5 and can be ignored until ACF6 exists
+// function include_field_types_fs_path_picker( $version ) {
 
-	include_once('acf-fs-path-picker-v5.php');
+// 	include_once('acf-fs-path-picker-v5.php');
 
-}
+// }
 
-add_action('acf/include_field_types', 'include_field_types_fs_path_picker');
+// add_action('acf/include_field_types', 'include_field_types_fs_path_picker');
 
 
